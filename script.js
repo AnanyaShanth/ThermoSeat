1cd95fcd52d886938331a492bb04f4265cbcee73
AnanyaShanth <ananyashanth93@gmail.com>
2025-08-09 06:59:39 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function handleStart() {
+    alert("ThermoSeat tracking initiated! Let's get warm.");
+    // You can redirect or load next page here if needed
+    // window.location.href = "dashboard.html";
+  }
+  
